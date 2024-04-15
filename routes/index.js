fb6f0f899b9aa94ad88776b2d5269ca9e205b2b6
danielgatschet <danielgatschet@gmail.com>
2024-04-15 15:02:50 -0500

    Working on part 3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Legumes App', users : req.user});
 });
 
-module.exports = router;
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Legumes App Registration'});
+});
+
+router.post('/register', function(req, res) {
+  Account.findOne({ username : req.body.username })
+    .then(function (user){
+      if(user != null ){
+        console.log("exists " + req.body.username)
+        return res.render('register', { title: 'Registration', message: 'Existing User', account : req.body.username })
+      }
+      let newAccount = new Account({ username : req.body.username });
+      Account.register(newAccount, req.body.password, function(err, user){
+        if (err) {
+          console.log("db creation issue "+ err)
+          return res.render('register', { title: 'Registration', message: 'access error', account : req.body.username })
+        }
+        if(!user){
+          return res.render('register',{ title: 'Registration', message: 'access error', account : req.body.username })
+        }
+      })
+      console.log('Sucess, redirect');
+      res.redirect('/');
+    })
+    .catch(function (err){
+      return res.render('register', { title: 'Registration', message: 'Registration error', account : req.body.username })
+    })
+});
