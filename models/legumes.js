fe0a52769caf1c80aa6d23f84ae757de71141132
danielgatschet <danielgatschet@gmail.com>
2024-04-17 12:48:11 -0500

    Finished part 5

--- models/legumes.js
+++ models/legumes.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const legumeSchema = mongoose.Schema({
     legume_type: String,
-    amount: Number,
+    amount: {type: Number, min: 1, max: 25},
     price: Number
 })
 module.exports = mongoose.model("Legumes", legumeSchema)
